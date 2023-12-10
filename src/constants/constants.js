39adcfe6f98eb535eea7ab1f21a13074f4f026bf
suryanarayanandv <dbvenkat857@gmail.com>
2023-12-11 00:54:29 +0530

    added project constants

---
+++ src/constants/constants.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview Constants used in the application.
+ */
+
+export const MAX_RELATED_PERSONS = 100000;
+
+export let RELATIONS = [];
+
+export let Person = {
+  id: -1,
+  name: "string",
+  age: "number",
+  parent: [],
+  sibilings: [],
+  children: [],
+  friends: [],
+};
